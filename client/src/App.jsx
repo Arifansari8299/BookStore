735ea6dc6d9762021fe687006749d0817d31a06c
Arif Ansari <arifkmclu@gmail.com>
2025-09-19 21:16:12 +0530

    Added Navbar from daisy ui component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div className='h-screen w-full flex justify-center items-center '>
-      <h1 className='text-3xl font-bold text-green-500 underline'>App</h1>
-    </div>
-  )
-}
+    <>
+      <Navbar />
+    </>
+  );
+};
 
-export default App
+export default App;
