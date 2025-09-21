6be7087b204d4ad0612241151bae4938530e77b6
Arif Ansari <arifkmclu@gmail.com>
2025-09-21 13:46:47 +0530

    change to es6 module

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
-const express = require('express')
+import express from 'express'
 const app = express()
-const dotenv = require('dotenv')
+import dotenv from 'dotenv'
 dotenv.config();
-const port = process.env.PORT
+const PORT = process.env.PORT || 4000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`server is  listening on port ${PORT}`)
 })
