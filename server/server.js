a872a5a2b1f12700610e1c9c4d5bcfbcb228819a
Arif Ansari <arifkmclu@gmail.com>
2025-09-21 13:57:19 +0530

    Mongodb connection code

--- server/server.js
+++ server/server.js
@@ -1,12 +1,18 @@
 import express from 'express'
+import mongoose from 'mongoose'
 const app = express()
 import dotenv from 'dotenv'
 dotenv.config();
 const PORT = process.env.PORT || 4000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const MONGO_URI = process.env.MONGO_URI
+// connect to MongoDB
+try{
+    mongoose.connect(MONGO_URI)
+    .then(()=>console.log("MongoDB Connected"))
+}
+catch(error){
+  console.log("error: ", error)
+}
 
 app.listen(PORT, () => {
   console.log(`server is  listening on port ${PORT}`)
